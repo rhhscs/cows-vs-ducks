0effc3f7915e24f035b2b209ebc79afb57309d7e
William Wu <williamwu277@gmail.com>
2023-09-03 16:37:54 -0400

    WaveManager

--- src/java/GameState/UI.java
+++ src/java/GameState/UI.java
@@ -8,7 +8,6 @@ import src.java.Utilities.ResolutionManager;
 public class UI {
     DocumentManager docs = new DocumentManager();
     CheerioManager cheerios = CheerioManager.getGlobalCheerios();
-    WaveManager waves = new WaveManager();
     Button pauseButton = new Button(ResolutionManager.WIDTH-120, 20, 80, 80);
     Button bookButton = new Button(ResolutionManager.WIDTH-220, 20, 80, 80);
 
@@ -21,7 +20,6 @@ public class UI {
     public void draw(Graphics g){
         docs.draw(g);
         cheerios.draw(g);
-        waves.draw(g);
         pauseButton.draw(g);
         bookButton.draw(g);
     }
